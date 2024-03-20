303bbcff035901b3cf70a7d5a620799819401bfc
dpiyumal2319 <132676592+dpiyumal2319@users.noreply.github.com>
2024-03-20 20:07:05 +0530

    Update App.java

--- POS/src/App.java
+++ POS/src/App.java
@@ -8,7 +8,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
-import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
